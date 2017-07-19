43a0b1e680c79e7cf1c480f30f8e558f2bd58990
Pushpraj Rajput <pushprajsinh.r@sigmainfo.net>
2017-07-19 23:17:56 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,49 @@
+var express = require('express');
+const bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient
+
+var URL = 'mongodb://localhost:27017/userdb'
+
+var app = express();
+
+app.set('port', process.env.PORT || 8081);
+app.set('view engine','ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+var users=[{
+    name:'Amit Dave',
+    email:'adave@gmail.com',
+    age:'25'
+},{
+    name:'Pushpraj Rajput',
+    email:'prajput@gmail.com',
+    age:'25'
+},{
+    name:'Suhit Rathi',
+    email:'srathi@gmail.com',
+    age:'25'
+}];
+
+MongoClient.connect(URL, function(err, db) {
+  if (err) return;
+
+app.get('/',function(req,res){
+    db.collection('users').find().toArray((err, data)=>{
+    if(err) console.log(err);
+    res.render('users',{users:data});
+    })
+});
+
+app.post('/user/save',function(req,res){
+    db.collection('users').save(req.body,(err, data)=>{
+    if(err) console.log(err);
+    res.redirect('/');
+    })
+});
+
+app.listen(8081,function(){
+    console.log("Application running on port 8081");
+});
+
+});
